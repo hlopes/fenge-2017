6b3fb6956648e205f933f3675ad4b52c514c5e86
Hugo Lopes <hugo.lopes@present-technologies.com>
2017-05-02 23:44:21 +0100

    phantom

--- wdio.conf.js
+++ wdio.conf.js
@@ -42,9 +42,9 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        maxInstances: 1,
         //
-        browserName: 'firefox'
+        browserName: 'phantomjs'
     }],
     //
     // ===================
@@ -106,7 +106,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone'],
+    services: ['phantomjs'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
